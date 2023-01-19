27f87dfaa211de69b449a600bd1ab856ec64a055
efecarranza <fcarr004@fiu.edu>
2023-01-19 17:40:23 -0500

    feat: update tests to be more thorough

--- src/test/ProposalPayloadE2E.t.sol
+++ src/test/ProposalPayloadE2E.t.sol
@@ -35,9 +35,9 @@ contract ProposalPayloadE2ETest is ProtocolV3TestBase {
         IReserveInterestRateStrategyV2(0xfC0Eace19AA7498e0f36eF1607D282a8d6debbDd);
 
     // New Strategies
-    address public constant NEW_INTEREST_RATE_STRATEGY_ETHEREUM = 0x04c28D6fE897859153eA753f986cc249Bf064f71;
-    address public constant NEW_INTEREST_RATE_STRATEGY_POLYGON = 0x80cb7e9E015C5331bF34e06de62443d070FD6654;
-    address public constant NEW_INTEREST_RATE_STRATEGY_POLYGON_V3 = 0x4b8D3277d49E114C8F2D6E0B2eD310e29226fe16;
+    address public NEW_INTEREST_RATE_STRATEGY_ETHEREUM;
+    address public NEW_INTEREST_RATE_STRATEGY_POLYGON;
+    address public NEW_INTEREST_RATE_STRATEGY_POLYGON_V3;
 
     // Util Addresses
     address public constant CROSSCHAIN_FORWARDER_POLYGON = 0x158a6bC04F0828318821baE797f50B0A1299d45b;
@@ -53,10 +53,9 @@ contract ProposalPayloadE2ETest is ProtocolV3TestBase {
     string internal POLYGON = AaveAddressBookV2.AaveV2Polygon;
 
     // New Strategies
-    IDefaultInterestRateStrategy public constant strategyPolygonV3 =
-        IDefaultInterestRateStrategy(NEW_INTEREST_RATE_STRATEGY_POLYGON_V3);
-    IReserveInterestRateStrategyV2 strategy = IReserveInterestRateStrategyV2(NEW_INTEREST_RATE_STRATEGY_ETHEREUM);
-    IReserveInterestRateStrategyV2 strategyPolygon = IReserveInterestRateStrategyV2(NEW_INTEREST_RATE_STRATEGY_POLYGON);
+    IReserveInterestRateStrategyV2 public strategy;
+    IReserveInterestRateStrategyV2 public strategyPolygon;
+    IDefaultInterestRateStrategy public strategyPolygonV3;
 
     function setUp() public {
         // To fork at a specific block: vm.createSelectFork(vm.rpcUrl("mainnet"), BLOCK_NUMBER);
@@ -66,9 +65,15 @@ contract ProposalPayloadE2ETest is ProtocolV3TestBase {
         // Deploy Payloads
         vm.selectFork(polygonFork);
         proposalPayloadPolygon = new ProposalPayloadPolygon();
+        NEW_INTEREST_RATE_STRATEGY_POLYGON = proposalPayloadPolygon.INTEREST_RATE_STRATEGY();
+        NEW_INTEREST_RATE_STRATEGY_POLYGON_V3 = proposalPayloadPolygon.INTEREST_RATE_STRATEGY_V3();
+        strategyPolygon = IReserveInterestRateStrategyV2(NEW_INTEREST_RATE_STRATEGY_POLYGON);
+        strategyPolygonV3 = IDefaultInterestRateStrategy(NEW_INTEREST_RATE_STRATEGY_POLYGON_V3);
 
         vm.selectFork(mainnetFork);
         proposalPayload = new ProposalPayload();
+        NEW_INTEREST_RATE_STRATEGY_ETHEREUM = proposalPayload.INTEREST_RATE_STRATEGY();
+        strategy = IReserveInterestRateStrategyV2(NEW_INTEREST_RATE_STRATEGY_ETHEREUM);
 
         // Create Proposal
         vm.prank(GovHelpers.AAVE_WHALE);
@@ -84,58 +89,75 @@ contract ProposalPayloadE2ETest is ProtocolV3TestBase {
         return input[64:];
     }
 
-    function testExecute() public {
+    function _modifyStrategy() internal {
+        // Pass vote and execute proposal
+        vm.selectFork(mainnetFork);
+        vm.recordLogs();
+        GovHelpers.passVoteAndExecute(vm, proposalId);
+
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        assertEq(keccak256("StateSynced(uint256,address,bytes)"), entries[5].topics[0]);
+        assertEq(address(uint160(uint256(entries[5].topics[2]))), FX_CHILD_ADDRESS);
+
+        // Mock the receive on L2 with the data emitted on StateSynced
+        vm.selectFork(polygonFork);
+        vm.startPrank(BRIDGE_ADMIN);
+        IStateReceiver(FX_CHILD_ADDRESS).onStateReceive(uint256(entries[5].topics[1]), this._cutBytes(entries[5].data));
+        vm.stopPrank();
+
+        // Forward time & execute proposal
+        BridgeExecutorHelpers.waitAndExecuteLatest(vm, POLYGON_BRIDGE_EXECUTOR);
+    }
+
+    function testExecuteValidateEthereum() public {
+        _modifyStrategy();
+        vm.selectFork(mainnetFork);
+
+        // Post-execution assertations
+        ReserveConfigV2[] memory allConfigsEthereum = AaveV2Helpers._getReservesConfigs(false, ETHEREUM);
+
+        ReserveConfigV2 memory expectedConfig = ReserveConfigV2({
+            symbol: "BAL",
+            underlying: BAL,
+            aToken: address(0), // Mock, no-validation because of the "dynamic" deployment on proposal execution
+            variableDebtToken: address(0), // Mock, no-validation because of the "dynamic" deployment on proposal exec
+            stableDebtToken: address(0), // Mock, no-validation because of the "dynamic" deployment on proposal execution
+            decimals: 18,
+            ltv: 6500,
+            liquidationThreshold: 7000,
+            liquidationBonus: 10800,
+            reserveFactor: 2000,
+            usageAsCollateralEnabled: true,
+            borrowingEnabled: false,
+            interestRateStrategy: NEW_INTEREST_RATE_STRATEGY_ETHEREUM,
+            stableBorrowRateEnabled: false,
+            isActive: true,
+            isFrozen: true
+        });
+
+        AaveV2Helpers._validateReserveConfig(expectedConfig, allConfigsEthereum);
+
+        AaveV2Helpers._validateInterestRateStrategy(
+            BAL,
+            ProposalPayload(proposalPayload).INTEREST_RATE_STRATEGY(),
+            InterestStrategyValuesV2({
+                excessUtilization: 20 * (AaveV2Helpers.RAY / 100),
+                optimalUtilization: 80 * (AaveV2Helpers.RAY / 100),
+                baseVariableBorrowRate: 3 * (AaveV2Helpers.RAY / 100),
+                stableRateSlope1: OLD_INTEREST_RATE_STRATEGY_ETHEREUM.stableRateSlope1(),
+                stableRateSlope2: OLD_INTEREST_RATE_STRATEGY_ETHEREUM.stableRateSlope2(),
+                variableRateSlope1: 14 * (AaveV2Helpers.RAY / 100),
+                variableRateSlope2: 150 * (AaveV2Helpers.RAY / 100)
+            }),
+            ETHEREUM
+        );
+    }
+
+    function testExecuteValidatePolygonV2() public {
         _modifyStrategy();
 
         // Post-execution assertations
         vm.selectFork(polygonFork);
-
-        ReserveConfig[] memory allConfigsAfterV3Polygon = _getReservesConfigs(AaveV3Polygon.POOL);
-
-        ReserveConfig memory expectedConfigPolygonV3 = ReserveConfig({
-            symbol: "BAL",
-            underlying: BAL_POLYGON,
-            aToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-            variableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-            stableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-            decimals: 18,
-            ltv: 2000,
-            liquidationThreshold: 4500,
-            liquidationBonus: 11000,
-            liquidationProtocolFee: 1000,
-            reserveFactor: 2000,
-            usageAsCollateralEnabled: true,
-            borrowingEnabled: true,
-            interestRateStrategy: _findReserveConfigBySymbol(allConfigsAfterV3Polygon, "BAL").interestRateStrategy,
-            stableBorrowRateEnabled: false,
-            isActive: true,
-            isFrozen: false,
-            isSiloed: false,
-            isBorrowableInIsolation: false,
-            isFlashloanable: false,
-            supplyCap: 284_600,
-            borrowCap: 256_140,
-            debtCeiling: 0,
-            eModeCategory: 0
-        });
-
-        _validateReserveConfig(expectedConfigPolygonV3, allConfigsAfterV3Polygon);
-
-        _validateInterestRateStrategy(
-            _findReserveConfigBySymbol(allConfigsAfterV3Polygon, "BAL").interestRateStrategy,
-            ProposalPayloadPolygon(proposalPayloadPolygon).INTEREST_RATE_STRATEGY_V3(),
-            InterestStrategyValues({
-                addressesProvider: address(AaveV3Polygon.POOL_ADDRESSES_PROVIDER),
-                optimalUsageRatio: 80 * (AaveV2Helpers.RAY / 100),
-                optimalStableToTotalDebtRatio: 20 * (AaveV2Helpers.RAY / 100),
-                baseStableBorrowRate: 16 * (AaveV2Helpers.RAY / 100),
-                stableRateSlope1: OLD_INTEREST_RATE_STRATEGY_V3_POLYGON.getStableRateSlope1(),
-                stableRateSlope2: OLD_INTEREST_RATE_STRATEGY_V3_POLYGON.getStableRateSlope2(),
-                baseVariableBorrowRate: 3 * (AaveV2Helpers.RAY / 100),
-                variableRateSlope1: 14 * (AaveV2Helpers.RAY / 100),
-                variableRateSlope2: 150 * (AaveV2Helpers.RAY / 100)
-            })
-        );
 
         ReserveConfigV2[] memory allConfigsPolygon = AaveV2Helpers._getReservesConfigs(false, POLYGON);
 
@@ -174,85 +196,65 @@ contract ProposalPayloadE2ETest is ProtocolV3TestBase {
             }),
             POLYGON
         );
-
-        vm.selectFork(mainnetFork);
-        ReserveConfigV2[] memory allConfigsEthereum = AaveV2Helpers._getReservesConfigs(false, ETHEREUM);
-
-        ReserveConfigV2 memory expectedConfig = ReserveConfigV2({
-            symbol: "BAL",
-            underlying: BAL,
-            aToken: address(0), // Mock, no-validation because of the "dynamic" deployment on proposal execution
-            variableDebtToken: address(0), // Mock, no-validation because of the "dynamic" deployment on proposal exec
-            stableDebtToken: address(0), // Mock, no-validation because of the "dynamic" deployment on proposal execution
-            decimals: 18,
-            ltv: 6500,
-            liquidationThreshold: 7000,
-            liquidationBonus: 10800,
-            reserveFactor: 2000,
-            usageAsCollateralEnabled: true,
-            borrowingEnabled: false,
-            interestRateStrategy: NEW_INTEREST_RATE_STRATEGY_ETHEREUM,
-            stableBorrowRateEnabled: false,
-            isActive: true,
-            isFrozen: true
-        });
-
-        AaveV2Helpers._validateReserveConfig(expectedConfig, allConfigsEthereum);
-
-        AaveV2Helpers._validateInterestRateStrategy(
-            BAL,
-            ProposalPayload(proposalPayload).INTEREST_RATE_STRATEGY(),
-            InterestStrategyValuesV2({
-                excessUtilization: 20 * (AaveV2Helpers.RAY / 100),
-                optimalUtilization: 80 * (AaveV2Helpers.RAY / 100),
-                baseVariableBorrowRate: 3 * (AaveV2Helpers.RAY / 100),
-                stableRateSlope1: OLD_INTEREST_RATE_STRATEGY_ETHEREUM.stableRateSlope1(),
-                stableRateSlope2: OLD_INTEREST_RATE_STRATEGY_ETHEREUM.stableRateSlope2(),
-                variableRateSlope1: 14 * (AaveV2Helpers.RAY / 100),
-                variableRateSlope2: 150 * (AaveV2Helpers.RAY / 100)
-            }),
-            ETHEREUM
-        );
-
-        // ReserveConfig memory expectedAssetConfig = ReserveConfig({
-        //     symbol: "BAL",
-        //     underlying: BAL_POLYGON,
-        //     aToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-        //     variableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-        //     stableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-        //     decimals: 18,
-        //     ltv: 6500,
-        //     liquidationThreshold: 7000,
-        //     liquidationBonus: 10800,
-        //     liquidationProtocolFee: 1000,
-        //     reserveFactor: 2000,
-        //     usageAsCollateralEnabled: true,
-        //     borrowingEnabled: false,
-        //     interestRateStrategy: _findReserveConfigBySymbol(allConfigsAfterV3Polygon, "BAL")
-        //         .interestRateStrategy,
-        //     stableBorrowRateEnabled: false,
-        //     isActive: true,
-        //     isFrozen: false,
-        //     isSiloed: false,
-        //     isBorrowableInIsolation: false,
-        //     isFlashloanable: false,
-        //     supplyCap: 100_000_000,
-        //     borrowCap: 256_140,
-        //     debtCeiling: 2_000_000_00,
-        //     eModeCategory: 1
-        // });
-
-        // InterestStrategyValues memory expectedStrategy = _getExpectedStrategyPolygon();
-
-        // _validateReserveConfig(expectedAssetConfig, allConfigsAfterV3Polygon);
-        // _validateInterestRateStrategy(
-        //     ProposalPayloadPolygon(proposalPayloadPolygon).INTEREST_RATE_STRATEGY(),
-        //     ProposalPayloadPolygon(proposalPayloadPolygon).INTEREST_RATE_STRATEGY(),
-        //     expectedStrategy
-        // );
     }
 
-    function testUtilizationAtZeroPercent() public {
+    function testExecuteValidatePolygonV3() public {
+        _modifyStrategy();
+
+        // Post-execution assertations
+        vm.selectFork(polygonFork);
+
+        ReserveConfig[] memory allConfigsAfterV3Polygon = _getReservesConfigs(AaveV3Polygon.POOL);
+
+        ReserveConfig memory expectedConfigPolygonV3 = ReserveConfig({
+            symbol: "BAL",
+            underlying: BAL_POLYGON,
+            aToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
+            variableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
+            stableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
+            decimals: 18,
+            ltv: 2000,
+            liquidationThreshold: 4500,
+            liquidationBonus: 11000,
+            liquidationProtocolFee: 1000,
+            reserveFactor: 2000,
+            usageAsCollateralEnabled: true,
+            borrowingEnabled: true,
+            interestRateStrategy: _findReserveConfigBySymbol(allConfigsAfterV3Polygon, "BAL").interestRateStrategy,
+            stableBorrowRateEnabled: false,
+            isActive: true,
+            isFrozen: false,
+            isSiloed: false,
+            isBorrowableInIsolation: false,
+            isFlashloanable: false,
+            supplyCap: 284_600,
+            borrowCap: proposalPayloadPolygon.NEW_BORROW_CAP_V3(),
+            debtCeiling: 0,
+            eModeCategory: 0
+        });
+
+        _validateReserveConfig(expectedConfigPolygonV3, allConfigsAfterV3Polygon);
+
+        _validateInterestRateStrategy(
+            _findReserveConfigBySymbol(allConfigsAfterV3Polygon, "BAL").interestRateStrategy,
+            ProposalPayloadPolygon(proposalPayloadPolygon).INTEREST_RATE_STRATEGY_V3(),
+            InterestStrategyValues({
+                addressesProvider: address(AaveV3Polygon.POOL_ADDRESSES_PROVIDER),
+                optimalUsageRatio: 80 * (AaveV2Helpers.RAY / 100),
+                optimalStableToTotalDebtRatio: 20 * (AaveV2Helpers.RAY / 100),
+                baseStableBorrowRate: 16 * (AaveV2Helpers.RAY / 100),
+                stableRateSlope1: OLD_INTEREST_RATE_STRATEGY_V3_POLYGON.getStableRateSlope1(),
+                stableRateSlope2: OLD_INTEREST_RATE_STRATEGY_V3_POLYGON.getStableRateSlope2(),
+                baseVariableBorrowRate: 3 * (AaveV2Helpers.RAY / 100),
+                variableRateSlope1: 14 * (AaveV2Helpers.RAY / 100),
+                variableRateSlope2: 150 * (AaveV2Helpers.RAY / 100)
+            })
+        );
+    }
+
+    // Interest Strategy Ethereum
+
+    function testUtilizationAtZeroPercentEthereum() public {
         (uint256 liqRate, uint256 stableRate, uint256 varRate) = strategy.calculateInterestRates(
             BAL,
             100 * 1e18,
@@ -268,7 +270,7 @@ contract ProposalPayloadE2ETest is ProtocolV3TestBase {
         assertEq(varRate, 3 * (AaveV2Helpers.RAY / 100));
     }
 
-    function testUtilizationAtOneHundredPercent() public {
+    function testUtilizationAtOneHundredPercentEthereum() public {
         (uint256 liqRate, uint256 stableRate, uint256 varRate) = strategy.calculateInterestRates(
             BAL,
             0,
@@ -284,7 +286,7 @@ contract ProposalPayloadE2ETest is ProtocolV3TestBase {
         assertEq(varRate, 1670000000000000000000000000);
     }
 
-    function testUtilizationAtUOptimal() public {
+    function testUtilizationAtUOptimalEthereum() public {
         (uint256 liqRate, uint256 stableRate, uint256 varRate) = strategy.calculateInterestRates(
             BAL,
             20 * 1e18,
@@ -300,38 +302,56 @@ contract ProposalPayloadE2ETest is ProtocolV3TestBase {
         assertEq(varRate, 17 * (AaveV2Helpers.RAY / 100));
     }
 
-    function _getExpectedStrategyPolygon() public view returns (InterestStrategyValues memory) {
-        return
-            InterestStrategyValues({
-                addressesProvider: address(OLD_INTEREST_RATE_STRATEGY_V3_POLYGON.ADDRESSES_PROVIDER()),
-                optimalStableToTotalDebtRatio: 20 * (RAY / 100),
-                optimalUsageRatio: 80 * (RAY / 100),
-                baseStableBorrowRate: 0,
-                baseVariableBorrowRate: 0,
-                stableRateSlope1: OLD_INTEREST_RATE_STRATEGY_V3_POLYGON.getStableRateSlope1(),
-                stableRateSlope2: 80 * (RAY / 100),
-                variableRateSlope1: 575 * (RAY / 10000),
-                variableRateSlope2: 80 * (RAY / 100)
-            });
+    // Interest Strategy Polygon V2
+
+    function testUtilizationAtZeroPercentPolygonV2() public {
+        vm.selectFork(polygonFork);
+        (uint256 liqRate, uint256 stableRate, uint256 varRate) = strategyPolygon.calculateInterestRates(
+            BAL,
+            100 * 1e18,
+            0,
+            0,
+            68200000000000000000000000,
+            2000
+        );
+
+        // At nothing borrowed, liquidity rate should be 0, variable rate should be 3% and stable rate should be 3%.
+        assertEq(liqRate, 0);
+        assertEq(stableRate, 0 * (AaveV2Helpers.RAY / 100));
+        assertEq(varRate, 3 * (AaveV2Helpers.RAY / 100));
     }
 
-    function _modifyStrategy() internal {
-        // Pass vote and execute proposal
-        vm.selectFork(mainnetFork);
-        vm.recordLogs();
-        GovHelpers.passVoteAndExecute(vm, proposalId);
-
-        Vm.Log[] memory entries = vm.getRecordedLogs();
-        assertEq(keccak256("StateSynced(uint256,address,bytes)"), entries[5].topics[0]);
-        assertEq(address(uint160(uint256(entries[5].topics[2]))), FX_CHILD_ADDRESS);
-
-        // Mock the receive on L2 with the data emitted on StateSynced
+    function testUtilizationAtOneHundredPercentPolygon() public {
         vm.selectFork(polygonFork);
-        vm.startPrank(BRIDGE_ADMIN);
-        IStateReceiver(FX_CHILD_ADDRESS).onStateReceive(uint256(entries[5].topics[1]), this._cutBytes(entries[5].data));
-        vm.stopPrank();
+        (uint256 liqRate, uint256 stableRate, uint256 varRate) = strategyPolygon.calculateInterestRates(
+            BAL,
+            0,
+            0,
+            100 * 1e18,
+            68200000000000000000000000,
+            2000
+        );
 
-        // Forward time & execute proposal
-        BridgeExecutorHelpers.waitAndExecuteLatest(vm, POLYGON_BRIDGE_EXECUTOR);
+        // At max borrow rate, stable rate should be 87% and variable rate should be 85.75%.
+        assertEq(liqRate, 1336000000000000000000000000);
+        assertEq(stableRate, 3100000000000000000000000000);
+        assertEq(varRate, 1670000000000000000000000000);
+    }
+
+    function testUtilizationAtUOptimalPolygon() public {
+        vm.selectFork(polygonFork);
+        (uint256 liqRate, uint256 stableRate, uint256 varRate) = strategyPolygon.calculateInterestRates(
+            BAL,
+            20 * 1e18,
+            0,
+            80 * 1e18,
+            68200000000000000000000000,
+            2000
+        );
+
+        // At UOptimal, stable rate should be % and variable rate should be 14%.
+        assertEq(liqRate, 108800000000000000000000000);
+        assertEq(stableRate, 10 * (AaveV2Helpers.RAY / 100));
+        assertEq(varRate, 17 * (AaveV2Helpers.RAY / 100));
     }
 }
